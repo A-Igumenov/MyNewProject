f61713a66f05aae95e1aaf0f9891bcf01d2b0801
A-Igumenov <47321233+A-Igumenov@users.noreply.github.com>
2021-02-10 10:48:03 +0200

    Program begin

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+
+int main() {
+  string eil, vardas;
+  eil = "* Sveiki, ";
+  cout<<"Iveskite savo varda: "; 
+  cin>>vardas; 
+  eil+=vardas+" *";
+
+  cout<<eil<<" "<< eil.size()<<endl;
+
+}
